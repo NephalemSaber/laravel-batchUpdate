6a8587ae41a3e071b584c95153c4936543538caa
nephalem.liu <nephalem.liu@feisu.com>
2021-10-21 17:26:37 +0800

    去掉多余代码

--- test/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once '../vendor/autoload.php';
-use Sabercode\LaravelBatchUpdate\BatchUpdate;
-
-$model = new BatchUpdate();
-$model->setTable('')->setWhereField('')->setData([])->doUpdate();
